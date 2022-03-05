215ef446af0a98165e072404a8afd50760fce9fd
Aman yadav <viratyaduvanshi007@gmail.com>
2022-03-06 00:15:41 +0530

    code

---
+++ College Programs/pattern1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+int n=7,x=1;
+
+   for(int i=2;i<=7;i++){
+    for(int j=i;j<=7;j++){
+        cout << j;
+    }
+    cout<<endl;
+   }
+}
