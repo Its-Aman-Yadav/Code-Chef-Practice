e753388f4653c0fcb18f9c1338ac32f10e3a1ca1
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2023-03-10 11:34:13 +0530

    Create Water Consumption.cpp

---
+++ Beginner Level/Water Consumption.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/WATERCONS
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t; cin>>t;
+	while(t--){
+	    int x; cin>>x;
+	    if(x>=2000) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+}
