552d2554d5a1fb23a0441ad5b2f9245d28845de3
Aman Yadav <97724135+Its-Aman-Yadav@users.noreply.github.com>
2023-03-10 11:23:29 +0530

    Create Add two Numbers.cpp

---
+++ Beginner Level/Add two Numbers.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/FLOW001
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+   int t;
+   cin>>t;
+   int a,b;
+   while(t--){
+       int sum=0;
+       cin>>a>>b;
+       sum=a+b;
+       cout<<sum<<"\n";
+   }
+   
+   return 0;
+}
