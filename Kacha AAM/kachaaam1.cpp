a68f5a9f6a35349ee52e636832f1ef4fa5f8ad34
Aman yadav <viratyaduvanshi007@gmail.com>
2023-05-03 20:37:07 +0530

    First commit

---
+++ Kacha AAM/kachaaam1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = 5;
+    if (a>3){
+        cout<<"Greater than 3";
+    }
+
+    else if(a<10){
+        cout<<"Hello";
+    }
+
+    else if(a>9){
+        cout<<"Hi";
+    }
+}
